9b6ec7e652d07f648071c9d64aec7a8d327442a1
lytemar <lytemar@yahoo.com>
2014-11-05 16:31:41 -0600

    Finish quiz 2 question 2

--- Practical_Machine_Learning/Quiz_2.R
+++ Practical_Machine_Learning/Quiz_2.R
@@ -2,6 +2,7 @@
 library(AppliedPredictiveModeling)
 library(caret)
 library(Hmisc)
+library(ggplot2)
 
 
 # Question 1
@@ -15,6 +16,15 @@ testing = adData[-trainIndex,]
 # Question 2
 data(concrete)
 set.seed(975)
+mixtures[,1:8] <- as.data.frame( lapply(mixtures[,1:8], cut2, g=10, levels.mean = TRUE) )
 inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
 training = mixtures[ inTrain,]
 testing = mixtures[-inTrain,]
+xTrain = seq(1,length(training$CompressiveStrength))
+xTest = seq(1,length(testing$CompressiveStrength))
+
+colors <- names(mixtures)
+colors <- colors[-9]
+g <- ggplot(training, aes(x = xTrain, y = CompressiveStrength, color = FlyAsh))
+g <- g + geom_point()
+print(g)
