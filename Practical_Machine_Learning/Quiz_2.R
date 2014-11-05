99ae48b9556c68213c736dfac277fa2064cae4aa
Mark R. Lytell <lytemar@yahoo.com>
2014-11-05 12:36:56 -0600

    Added Practical Machine Learning class directory

---
+++ Practical_Machine_Learning/Quiz_2.R
@@ -0,0 +1,20 @@
+# Quiz 2
+
+# Question 1
+library(AppliedPredictiveModeling)
+library(caret)
+data(AlzheimerDisease)
+
+adData = data.frame(diagnosis,predictors)
+trainIndex = createDataPartition(diagnosis,p=0.5,list=FALSE)
+training = adData[trainIndex,]
+testing = adData[-trainIndex,]
+
+# Question 2
+library(AppliedPredictiveModeling)
+data(concrete)
+library(caret)
+set.seed(975)
+inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
+training = mixtures[ inTrain,]
+testing = mixtures[-inTrain,]
