3c41e0da6daf62ff3ebb52d3dd769ded87aa04cc
lytemar@yahoo.com <lytemar@yahoo.com>
2014-11-06 20:07:59 -0600

    Update Quiz 4

--- Practical_Machine_Learning/Quiz_4.R
+++ Practical_Machine_Learning/Quiz_4.R
@@ -3,4 +3,10 @@
 # Question 1
 library(ElemStatLearn)
 data(vowel.train)
-data(vowel.test) 
+data(vowel.test)
+
+vowel.train$y <- as.factor(vowel.train$y)
+vowel.test$y <- as.factor(vowel.test$y)
+set.seed(33833)
+modFitRF <- train(y ~ ., method="rf", prox=TRUE, data=vowel.train)
+modFitBoost <- train(y ~ ., method="gbm", data=vowel.train, verbose=FALSE)
