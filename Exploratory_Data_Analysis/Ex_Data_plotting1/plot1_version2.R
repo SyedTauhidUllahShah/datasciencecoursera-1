69c3c7fe807ae1193ee7ee897a04448057807dbf
lytemar@yahoo.com <lytemar@yahoo.com>
2014-11-09 21:25:16 -0600

    First commit of project

--- Exploratory_Data_Analysis/Ex_Data_plotting1/plot1_version2.R
+++
@@ -1,4 +0,0 @@
-data <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=FALSE, sep=';')
-png(filename='plot1_version2.png', width=480, height=480)
-hist(data$V3, col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)',bg = "transparent")
-dev.off()
--- Reproducable_Research/dev_script.R
+++
@@ -1,6 +0,0 @@
-library(plyr)
-unzip("repdata-data-activity.zip")
-data <- read.csv("activity.csv")
-
-data$date <- as.Date(data$date)
-TotalSteps <-ddply(data, .(date), summarize, total_steps=sum(steps))
