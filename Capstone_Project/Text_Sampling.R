97fe64cd69386a582fcffabff4567347c274377d
lytemar@yahoo.com <lytemar@yahoo.com>
2015-03-16 19:40:40 -0500

    Created text sampling script

---
+++ Capstone_Project/Text_Sampling.R
@@ -0,0 +1,35 @@
+# text sampling for the english corpora
+# uses binomial sampling to select the lines to read in
+
+numLinesTwitter <- 2360148
+numLinesBlogs <- 899288
+numLinesNews <- 1010242
+
+fileDir <- "~/Documents/School/Coursera Data Science/Capstone Project/final/en_US/"
+TwitterStr <- paste0(fileDir, "en_US.twitter.txt")
+newsStr <- paste0(fileDir, "en_US.news.txt")
+blogsStr <- paste0(fileDir, "en_US.blogs.txt")
+
+textSample <- function(fileStr, nLines, p = .25){
+     
+     filePath <- paste0(fileDir, fileStr)
+     
+     # create a connection for the file
+     con <- file(filePath, open="r")
+
+     # read in the lines and close the connection
+     lines <- readLines(con) 
+     close(con)
+
+     # Generate random sample
+     set.seed(12321)
+     samples <- rbinom(n = nLines, size = 1, prob = p)
+     lines <- lines[samples]
+
+     # write the lines to a file
+     filePath <- paste0(fileDir,"sampled/",fileStr)
+     con <- file(filePath, open="w")
+
+     writeLines(lines, con)
+     close(con)
+}
